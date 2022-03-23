6ccef14480e7d551a95693ee7e85908dc5eaec01
jam65st <jam65st@gmail.com>
2022-03-23 17:52:54 -0500

    First test: Setting up a TS project with IntelliJ IDEA

---
+++ dev/app.ts
@@ -0,0 +1,4 @@
+const a = "1";
+console.log( `Say hello world!!! ${a}`);
+// console.error( 'HAS type error', a.foo() );
+console.error( 'NOT type error', a );
---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const a = "1";
+console.log(`Say hello world!!! ${a}`);
+// console.error( 'HAS type error', a.foo() );
+console.error('NOT type error', a);
+//# sourceMappingURL=app.js.map
